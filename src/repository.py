1c5fb5492a60b11944916b3dc3b00cfc5a755f8b
AnisShaikh10 <anisofficial0208@gmail.com>
2023-02-02 18:07:56 +0530

    Updated files

--- src/repository.py
+++ src/repository.py
@@ -3,13 +3,13 @@ import json
 
 class Repository:
 	DBPATH = "./todo.db"
-  Balance = 0
-  status = False
+	Balance = 0
+	status = False
 
 	@staticmethod
 	def connect_db():
 		return sqlite3.connect(Repository.DBPATH)
-	
+
 	@staticmethod
 	def add_loan_account(loan_amt, time_period, interest_rate, account_num):
 		try:
@@ -32,7 +32,7 @@ class Repository:
 			}
 		except Exception as e:
 			raise Exception("Error: ", e)
-			
+
 	@staticmethod
 	def get_loan_account(id):
 		try:
@@ -42,7 +42,7 @@ class Repository:
 			return row
 		except Exception as e:
 			raise Exception("Error: ", e)
-	
+
 	@staticmethod
 	def get_all_loan_accounts():
 		try:
@@ -52,7 +52,7 @@ class Repository:
 			return rows
 		except Exception as e:
 			raise Exception("Error: ", e)
-	
+
 	@staticmethod
 	def get_all_loan_ammount_sum():
 		try:
@@ -62,7 +62,7 @@ class Repository:
 			return row
 		except Exception as e:
 			raise Exception("Error: ", e)
-	
+
 	@staticmethod
 	def add_loan_deposit(deposit_amt, loan_id):
 		try:
@@ -104,93 +104,94 @@ class Repository:
 		except Exception as e:
 			raise Exception("Error: ", e)
 
-    @staticmethod
-    def create_account(name, email, mob, city):
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            insert_cursor = c.execute('INSERT INTO Accounts (userName, emailID, mobileNum, city, balance) VALUES (?, ?, ?, ?, ?)',
-            (name, email, mob, city, Repository.Balance))
-            conn.commit()
-            return{
-                'accountNum': insert_cursor.lastrowid,
-                'userName': name,
-                'emailID': email,
-                'mobileNum': mob,
-                'city': city,
-                'balance': Repository.Balance
-            }
-        except Exception as e:
-            raise Exception('Error: ', e)
+	@staticmethod
+	def create_account(name, email, mob, city):
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			insert_cursor = c.execute('INSERT INTO Accounts (userName, emailID, mobileNum, city, balance) VALUES (?, ?, ?, ?, ?)',
+			(name, email, mob, city, Repository.Balance))
+			conn.commit()
+			return{
+				'accountNum': insert_cursor.lastrowid,
+				'userName': name,
+				'emailID': email,
+				'mobileNum': mob,
+				'city': city,
+				'balance': Repository.Balance
+			}
+		except Exception as e:
+			raise Exception('Error: ', e)
 
-    @staticmethod
-    def get_account_details(bank_acc_no):
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            rows = c.execute('select * from Accounts where status=true AND accountNum=?',(bank_acc_no, ))
-            print("Rows Value: " ,rows)
-            return rows
-        except Exception as e:
-            raise Exception('Error: ',e)
+	@staticmethod
+	def get_account_details(bank_acc_no):
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from Accounts where status=true AND accountNum=?',(bank_acc_no, ))
+			print("Rows Value: " ,rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ',e)
 
-    @staticmethod
-    def get_all_account_details():
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            rows = c.execute('select * from Accounts where status=true')
-            print("Rows Value: " ,rows)
-            return rows
-        except Exception as e:
-            raise Exception('Error: ',e)
+	@staticmethod
+	def get_all_account_details():
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from Accounts where status=true')
+			print("Rows Value: " ,rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ',e)
 
-    @staticmethod
-    def deposite_amount(bank_acc_no, amount):
+	@staticmethod
+	def deposite_amount(bank_acc_no, amount):
 
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            fetch_cursor = c.execute('select balance from Accounts where accountNum=?', (bank_acc_no, ))
-            balance = fetch_cursor.fetchone()[0]
-            update_balance =  balance + amount
-            insert_cursor = c.execute('INSERT INTO Deposits (accountNum, depositAmount) VALUES(?, ?)',
-            (bank_acc_no, amount))
-            update_cursor = c.execute('UPDATE Accounts SET balance=? WHERE accountNum=?', (update_balance, bank_acc_no, ))
-            conn.commit()
-            return{
-                'id': insert_cursor.lastrowid,
-                'Bank_Account_Number': bank_acc_no,
-                'Amount': amount
-            }
-        except Exception as e:
-            raise Exception('Error: ', e)
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			fetch_cursor = c.execute('select balance from Accounts where accountNum=?', (bank_acc_no, ))
+			balance = fetch_cursor.fetchone()[0]
+			update_balance =  balance + amount
+			insert_cursor = c.execute('INSERT INTO Deposits (accountNum, depositAmount) VALUES(?, ?)',
+			(bank_acc_no, amount))
+			update_cursor = c.execute('UPDATE Accounts SET balance=? WHERE accountNum=?', (update_balance, bank_acc_no, ))
+			conn.commit()
+			return{
+				'id': insert_cursor.lastrowid,
+				'Bank_Account_Number': bank_acc_no,
+				'Amount': amount
+			}
+		except Exception as e:
+			raise Exception('Error: ', e)
 
-    @staticmethod
-    def get_deposite_amount(bank_acc_no):
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            rows = c.execute('select * from Deposits where accountNum=?', (bank_acc_no, ))
-            # print("Row Values:",rows)
-            return rows
-        except Exception as e:
-            raise Exception('Error: ', e)
+	@staticmethod
+	def get_deposite_amount(bank_acc_no):
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from Deposits where accountNum=?', (bank_acc_no, ))
+			# print("Row Values:",rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ', e)
 
 
-    @staticmethod
-    def delete_user(bank_acc_no):
-        try:
-            conn = Repository.connect_db()
-            c = conn.cursor()
-            fetch_status = c.execute('select status from Accounts where accountNum=?', (bank_acc_no, ))
-            if fetch_status == 0:
-                raise TypeError("Account Dosen't Exist")
-            rows = c.execute('UPDATE Accounts SET status=? WHERE accountNum=?', (0, bank_acc_no, ))
-            conn.commit()
-            return{
-                'Bank_Account_Number': bank_acc_no,
-                'status': False
-            }
-        except Exception as e:
-            raise Exception('Error: ', e)
+	@staticmethod
+	def delete_user(bank_acc_no):
+		try:
+			conn = Repository.connect_db()
+			c = conn.cursor()
+			fetch_status = c.execute('select status from Accounts where accountNum=?', (bank_acc_no, ))
+			# print(fetch_status)
+			if fetch_status.fetchone()[0] == 0:
+				raise TypeError("Account Dosen't Exist")
+			rows = c.execute('UPDATE Accounts SET status=? WHERE accountNum=?', (0, bank_acc_no, ))
+			conn.commit()
+			return{
+				'Bank_Account_Number': bank_acc_no,
+				'status': False
+			}
+		except Exception as e:
+			raise Exception('Error: ', e)
